6e7a4000a2c09c445c3da1dbbb89ad24acb5fa7d
Ruzana khan <90472334+Ruzana-khan@users.noreply.github.com>
2022-01-12 23:18:13 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+var typed = new Typed('.test', {
+    
+    strings: ['Runa khan',
+    'is a youtube channel',
+     'provide videos about online earning and computer tips & tricks',
+    'also share some other informations about modern technology'],
+    typeSpeed:40,
+    backSpeed:24,
+    startDelay:1500,
+    loop:true,
+  });
